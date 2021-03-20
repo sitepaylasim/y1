ebf41c2538fd194e90eeb8ce32c3aa1e8add7cfc
sitepaylasim <80833836+sitepaylasim@users.noreply.github.com>
2021-03-20 23:24:10 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '709069396353613874', // sunucu idsi
+  ownerRole:"709069702659309568", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[716722516701478923], // kayıt olmayanlara verieln rol
+  jailRolu:"800204633435537439" , // cezalılara verilen rol
+  enAltYetkiliRolu:[788399644875030538], // ilk yetkili permi
+  activity:"𐌞anity Stat", // botun durumu
+  status:"idle",
+  invitelink:"", //davet linki
+  symbol:"𐌞", // tag
+  voicechannel:"748289834526703767" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
